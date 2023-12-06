ac54a5161b0b5a9f3b475f9acf82c8e703d67a3f
JoaoVictorRodrigues <jvictor.rodrigues08@gmail.com>
2023-12-05 22:57:29 -0300

    Classe AsyncFaceStudy com callback

--- FaceStudy/FaceStudy.py
+++ FaceStudy/FaceStudy.py
@@ -1,6 +1,6 @@
-from deepface import DeepFace
+import asyncio
 import cv2
-import time
+from deepface import DeepFace
 
 class FaceStudy:
     def __init__(self,analyze_type,window):
@@ -36,12 +36,46 @@ class FaceStudy:
 
         cap.release()
         cv2.destroyAllWindows()
-    
-    def verify():
-        pass
-    def find():
-        pass
-            
-    def represent():
-        pass
 
+class AsyncFaceStudy:
+    def __init__(self, analyze_type, window):
+        self.action_name = analyze_type
+        self.window_title = window
+
+    async def analyze(self, frame):
+        loop = asyncio.get_event_loop()
+        result = await loop.run_in_executor(None, self._analyze_sync, frame)
+        return result
+
+    def _analyze_sync(self, frame):
+        try:
+            result = DeepFace.analyze(frame, actions=[self.action_name], enforce_detection=False)
+            if self.action_name != 'age':
+                return result[0][f'dominant_{self.action_name.lower()}']
+            else:
+                return result[0][f'{self.action_name.lower()}']
+        except ValueError as e:
+            print(f"Error: {e}")
+            return "Not detected"
+
+    async def run(self, result_callback):
+        cap = cv2.VideoCapture(0)
+
+        while True:
+            ret, frame = cap.read()
+
+            result = await self.analyze(frame)
+            print(f"Dominant {self.action_name}: {result}")
+
+            # Call the callback function with the result
+            await result_callback(self.action_name, result)
+
+            cv2.putText(frame, str(result), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+            cv2.imshow(self.window_title, frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
