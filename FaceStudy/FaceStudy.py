55e572cd64a518fccc8684644bb3f369f7f199ba
timas <guilhermeaugustochaves@hotmail.com>
2023-12-07 23:59:28 -0300

    consertando funcoes assincronas com e sem callback

--- FaceStudy/FaceStudy.py
+++ FaceStudy/FaceStudy.py
@@ -2,45 +2,11 @@ import asyncio
 import cv2
 from deepface import DeepFace
 
-class FaceStudy:
-    def __init__(self,analyze_type,window):
-        self.action_name = analyze_type
-        self.window_title = window
-
-    def analyze(self, frame):
-        try:
-            result = DeepFace.analyze(frame, actions=[self.action_name], enforce_detection=False)
-            #time.sleep(0.1)
-            if(self.action_name != 'age'):
-                return result[0][f'dominant_{self.action_name.lower()}']
-            else:
-                return result[0][f'{self.action_name.lower()}']
-        except ValueError as e:
-            print(f"Error: {e}")
-            return "Not detected"   
-        
-    def run(self):
-        cap = cv2.VideoCapture(0)
-
-        while True:
-            ret, frame = cap.read()
-
-            result = self.analyze(frame)
-            print(f"Dominant {self.action_name}: {result}")
-            cv2.putText(frame, str(result), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-
-            cv2.imshow(self.window_title, frame)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        cap.release()
-        cv2.destroyAllWindows()
-
 class AsyncFaceStudy:
-    def __init__(self, analyze_type, window):
+    def __init__(self, analyze_type, window, result_callback=None):
         self.action_name = analyze_type
         self.window_title = window
+        self.result_callback = result_callback
 
     async def analyze(self, frame):
         loop = asyncio.get_event_loop()
@@ -58,7 +24,7 @@ class AsyncFaceStudy:
             print(f"Error: {e}")
             return "Not detected"
 
-    async def run(self, result_callback):
+    async def run(self):
         cap = cv2.VideoCapture(0)
 
         while True:
@@ -67,8 +33,48 @@ class AsyncFaceStudy:
             result = await self.analyze(frame)
             print(f"Dominant {self.action_name}: {result}")
 
-            # Call the callback function with the result
-            await result_callback(self.action_name, result)
+            if self.result_callback:
+                # Chama o callback se fornecido
+                await self.result_callback(self.action_name, result)
+
+            cv2.putText(frame, str(result), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+            cv2.imshow(self.window_title, frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+    # Adicione a função result_callback aqui
+    async def result_callback(self, action_name, result):
+        print(f"Callback: Dominant {action_name}: {result}")
+
+class SyncFaceStudy:
+    def __init__(self, analyze_type, window):
+        self.action_name = analyze_type
+        self.window_title = window
+
+    def analyze(self, frame):
+        try:
+            result = DeepFace.analyze(frame, actions=[self.action_name], enforce_detection=False)
+            if self.action_name != 'age':
+                return result[0][f'dominant_{self.action_name.lower()}']
+            else:
+                return result[0][f'{self.action_name.lower()}']
+        except ValueError as e:
+            print(f"Error: {e}")
+            return "Not detected"
+
+    def run(self):
+        cap = cv2.VideoCapture(0)
+
+        while True:
+            ret, frame = cap.read()
+
+            result = self.analyze(frame)
+            print(f"Dominant {self.action_name}: {result}")
 
             cv2.putText(frame, str(result), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
---
+++ FaceStudy/test.py
@@ -0,0 +1,22 @@
+import asyncio
+from FaceStudy import AsyncFaceStudy, SyncFaceStudy
+
+# Função de teste para a versão assíncrona com callback
+async def test_async_with_callback():
+    async_face_study = AsyncFaceStudy(analyze_type='emotion', window='Async With Callback')
+    await async_face_study.run()
+
+# Função de teste para a versão assíncrona sem callback
+async def test_async_without_callback():
+    async_face_study = AsyncFaceStudy(analyze_type='emotion', window='Async Without Callback')
+    await async_face_study.run()
+
+# Função de teste para a versão síncrona
+def test_sync():
+    sync_face_study = SyncFaceStudy(analyze_type='emotion', window='Sync Face Study')
+    sync_face_study.run()
+
+# Executar os testes
+asyncio.run(test_async_with_callback())
+asyncio.run(test_async_without_callback())
+test_sync()
