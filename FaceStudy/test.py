f022993f26f9565144dbde4af35248e6ee2807d5
GabrielaGiosa <gabichoichit@gmail.com>
2023-12-11 21:14:50 -0300

    docs

--- FaceStudy/test.py
+++ FaceStudy/test.py
@@ -3,17 +3,17 @@ from FaceStudy import AsyncFaceStudy, SyncFaceStudy
 
 # Função de teste para a versão assíncrona com callback
 async def test_async_with_callback():
-    async_face_study = AsyncFaceStudy(analyze_type='emotion', window='Async With Callback')
+    async_face_study = AsyncFaceStudy(analyze_type='age', window='Async With Callback')
     await async_face_study.run()
 
 # Função de teste para a versão assíncrona sem callback
 async def test_async_without_callback():
-    async_face_study = AsyncFaceStudy(analyze_type='emotion', window='Async Without Callback')
+    async_face_study = AsyncFaceStudy(analyze_type='age', window='Async Without Callback')
     await async_face_study.run()
 
 # Função de teste para a versão síncrona
 def test_sync():
-    sync_face_study = SyncFaceStudy(analyze_type='emotion', window='Sync Face Study')
+    sync_face_study = SyncFaceStudy(analyze_type='age', window='Sync Face Study')
     sync_face_study.run()
 
 # Executar os testes
